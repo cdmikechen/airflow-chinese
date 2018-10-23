3a45bee11d4525fe20f0bb4525fad6d2704a9db2
chenx <chenx@neunn.com>
2018-10-23 16:36:50 +0800

    1.9版本修改

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # airflow-chinese
-airflow本地汉化以及时区适配
+
+该版本取自 https://github.com/cdmikechen/incubator-airflow/tree/v1-9-neunn 里面的内容。
+
+版本内除了针对前台的汉化以外还修改了部分后台的代码，主要针对后台的时间做了适配，但是并不是很成熟，仅可以作为学习和交流使用。
---
+++ utils/move_html.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/dag.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/dag_bak.html
+\cp -rf dag.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/dag.html
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/dags.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/dags_bak.html
+\cp -rf dags.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/dags.html
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/chart.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/chart_bak.html
+\cp -rf chart.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/chart.html
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/dag_details.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/dag_details_bak.html
+\cp -rf dag_details.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/dag_details.html
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/duration_chart.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/duration_chart_bak.html
+\cp -rf duration_chart.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/duration_chart.html
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/gantt.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/gantt_bak.html
+\cp -rf gantt.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/gantt.html
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/graph.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/graph_bak.html
+\cp -rf graph.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/graph.html
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/task.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/task_bak.html
+\cp -rf task.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/task.html
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/task_instance.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/task_instance_bak.html
+\cp -rf task_instance.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/task_instance.html
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/tree.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/tree_bak.html
+\cp -rf tree.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/tree.html
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/tree.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/tree_bak.html
+\cp -rf tree.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/airflow/tree.html
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/static/main.css /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/static/main_bak.css
+\cp -rf main.css /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/static/main.css
+
+\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/admin/master.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/admin/master_bak.html
+\cp -rf master.html /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/templates/admin/master.html
+
+#\cp -rf /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/views.py /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/views_back.py
+\cp -rf views.py /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/views.py
+
+\cp -rf utils.py /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/www/utils.py
+
+\cp -rf jobs.py /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/jobs.py
+
+\cp -rf models.py /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/models.py
+
+\cp -rf dates.py /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/utils/dates.py
+
+\cp -rf prev_dagrun_dep.py /home/python/soft_install/python3.6/lib/python3.6/site-packages/airflow/ti_deps/deps/prev_dagrun_dep.py
